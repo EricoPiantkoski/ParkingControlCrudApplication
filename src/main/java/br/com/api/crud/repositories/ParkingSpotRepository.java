c29a340f15d50c63c401442c268091b25709605d
Erico Piantkoski <ericopiantkoski@gmail.com>
2023-01-01 21:28:23 -0300

    add parking spot repository and getters and setters to model

---
+++ src/main/java/br/com/api/crud/repositories/ParkingSpotRepository.java
@@ -0,0 +1,9 @@
+package br.com.api.crud.repositories;
+
+import br.com.api.crud.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+}
